e8b76456bcba5ced736844bc3ba5a103d5d9d3e2
Valery <mr.robariii@gmail.com>
2023-04-28 09:29:31 +0200

    working setup

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,80 @@
 use std::io::{Read, Write};
-use std::str::FromStr;
 use std::time::Duration;
-use std::thread::sleep;
-use icu_timezone::{CustomTimeZone, MetazoneCalculator, MetazoneId, TimeZoneBcp47Id};
 use nmea_parser::{NmeaParser, ParsedMessage};
-use nmea_parser::chrono::format::parse;
-use serialport::{SerialPort, SerialPortInfo};
 use tzf_rs::{DefaultFinder, deg2num};
 
 struct Parser{
-    byte_counter: i32,
+    position_index: i32,
     serial_buf: [u8; 1024],
     parser: NmeaParser,
     finder: DefaultFinder,
-    connected: bool,
-    can_iterate: bool,
-    buffer_ordinary: [u8; 512]
 }
 
-impl Parser {
-    fn form_sentence(&mut self, t: usize){
-        let slice = &self.serial_buf[..t];
-        for value in slice.iter(){
-            let symbol = char::from(*value);
-
-            if *value == b'\r' // чекнуть вот тут. CR character
-            {
-                continue;
-            }
-            if *value == b'\n' { // чекнуть вот тут. LF character
-
-                let line = String::from_utf8_lossy(&self.buffer_ordinary[0..self.byte_counter as usize]);
-                println!("Formed line {}", line);
-                self.byte_counter = 0; // нужная херня, без неё не обнуляется просто счетчик вообще.
-
-                match self.parser.parse_sentence(&line) {
-                    Ok(sentence) => {
-                        self.retrieve_data_from_sentence(sentence);
-                        return;
-                    },
-                    Err(error) => {
-                    }
-                }
-            }
-            self.buffer_ordinary[self.byte_counter as usize] = *value;
-            self.byte_counter += 1;
-        }
-    }
-
-    fn retrieve_data_from_sentence(&mut self, sentence: ParsedMessage){
-        match sentence {
-            ParsedMessage::Gll(gll) => {
-                println!("Navigation: {:?}", gll);
-            }
-            ParsedMessage::Rmc(rmc) => {
-                if let Some(lon) = rmc.longitude {
-                    if let Some(lat) = rmc.latitude {
-                        println!("RMC pos: {} {}", lon, lat);
-                        let timezone = self.finder.get_tz_name(rmc.longitude.unwrap(), rmc.latitude.unwrap());
-                        println!("Time: {}", timezone);
-                        if let Some(timestamp) = rmc.timestamp {
-                            println!("{:?} \n", timestamp);
-                        }
-                    }
-                }
-            },
-            _ => {}
-        }
+impl  Parser {
+    fn form_sentence(){
+        
     }
 }
 
 fn main() {
-    let mut parser_struct = Parser{
-        byte_counter: 0,
-        serial_buf: [0u8; 1024],
-        parser: NmeaParser::new(),
-        finder: DefaultFinder::new(),
-        connected: false,
-        can_iterate: true,
-        buffer_ordinary: [0u8; 512],
-    };
-
     let ports = serialport::available_ports().expect("System error");
     let port = ports.first().expect("No ports available");
     println!("Receiving data on {} at {} baud:", &port.port_name, 9600);
-    let mut port= serialport::new(&port.port_name, 9600)
-    .timeout(Duration::from_millis(10))
+
+    let mut port = serialport::new(&port.port_name, 9600)
+        .timeout(Duration::from_millis(10))
         .open()
         .expect(&format!("Unable to open serial port '{}'", port.port_name));
 
-    let mut h = 0;
-    let mut m = 0;
-    let mut s = 0;
+    let mut buffer = [0u8; 1024];
+    let mut pos = 0;
 
-    let delay = Duration::from_secs(1);
+    let mut parser = NmeaParser::new();
+    let mut finder = DefaultFinder::new();
 
+    let mut serial_buf = [0u8; 1024];
 
     loop {
-        parser_struct.byte_counter = 0; // нужная херня, без неё не отлавливаются корректные сообщения в принципе
-        match port.read(parser_struct.serial_buf.as_mut_slice()) {
+        match port.read(serial_buf.as_mut_slice()) {
             Ok(t) => {
-                parser_struct.form_sentence(t);
-                sleep(delay)
-                // std::io::stdout().write_all(&serial_buf[..t]).unwrap()
-            },
+                let slice = &serial_buf[..t];
 
+                for b in slice { // На контроллере будет просто чтение по одному байту
+                    if *b != b'\n' {
+                        if *b == b'\r' {
+                            let end = pos;
+                            pos = 0;
+                            let line = core::str::from_utf8(&buffer[0..end]).unwrap();
+                            if line.starts_with('$') {
+                                // println!("Line:    {:?}", line);
+                                if let Ok(sentence) = parser.parse_sentence(line) {
+                                    //println!("Line:    {:?}", sentence);
+                                    match sentence {
+                                        ParsedMessage::Rmc(rmc) => {
+                                            if let Some(lon) = rmc.longitude {
+                                                if let Some(lat) = rmc.latitude {
+                                                    println!("RMC pos: {} {}", lon, lat);
+                                                    let timezone = finder.get_tz_name(rmc.longitude.unwrap(), rmc.latitude.unwrap());
+                                                    println!("Time: {}", timezone);
+                                                    if let Some(timestamp) = rmc.timestamp {
+                                                        println!("{:?} \n", timestamp);
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        _ => {}
+                                    }
+                                }
+                            } else {
+                                eprintln!("Broken:  {:?}", line);
+                            }
+                        } else {
+                            buffer[pos] = *b;
+                            pos += 1;
+                        }
+                    }
+                }
+            },
             Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
             other => {
                 other.unwrap();
@@ -112,20 +82,3 @@ fn main() {
         }
     }
 }
-
-//Високосные года!!
-
-// for (i, b) in buffer.iter().enumerate() {
-//     match i {
-//         0 => h = 10 * (*b - b'0'), //ASCII TO NUMBER HACK
-//         1 => h += (*b - b'0'),
-//         2 => m = 10 * (*b - b'0'),
-//         3 => m += (*b - b'0'), //..
-//         4 => s += 10 * (*b - b'0'),
-//         5 => s += (*b - b'0'),
-//         _ => {}
-//     }
-// }
-
-//USART по пину (TX, RX на картинке)
-//https://github.com/stm32-rs/stm32f4xx-hal/blob/master/examples/rtic-usart-shell.rs
